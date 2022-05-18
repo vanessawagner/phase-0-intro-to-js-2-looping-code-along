04ad429f90046303dc60d458a636c7d037ed9eb7
Vanessa Wagner <vmwagner@gmail.com>
2022-05-18 18:38:15 -0500

    commit tree 636fe9fc97916520c1f1615b7010c7b033656a49

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+
+    for (let i=0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        
+    }
+    return messages
+
+}
+
+function countDown() {
+for (let countDown = 10; countDown >= 0; countDown--) 
+    console.log(countDown);
+}
